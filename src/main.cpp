58b492adb14abcc8998342046ebab8f35c47703e
Ladapn <2048042+ladapn@users.noreply.github.com>
2020-02-12 22:06:08 +0100

    Add RC car functionality
    Connect the BLE joystick decoder functionality with RC car control functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,155 @@
 #include <Arduino.h>
+#include <Servo.h>
 #include "BLEJoystickDecoder.h"
 
+const int PWM_A   = 3;
+const int DIR_A   = 12;
+const int BRAKE_A = 9;
+const int SNS_A   = A0;
+const int SERVO_A = 47;
+
+const int MAX_SPD = 255;  //200; //TBD
+const int SPD_INCREMENT = 50;
+
+const int SERVO_INCREMENT = 10;
+const int SERVO_CENTER = 80;
+const int SERVO_MIN_RIGHT = 20;
+const int SERVO_MAX_LEFT = 160;
+
+const int STATUS_PRINT_INTERVAL_MS = 1000;
+
+const int SNS_BATTERY_VLTG = A14;
+
+Servo myservo;  // create servo object to control a servo
+
+// TODO: class to encapsulate motor
+bool motorCMD(int commandSPD)
+{
+  bool retval = false;
+  bool motorDirection = HIGH; // HIGH -> forward
+
+  if((commandSPD <= 255) && (commandSPD >= -255))
+  {
+    if(commandSPD < 0)
+    {
+      motorDirection = LOW;
+      commandSPD = abs(commandSPD);
+    }
+
+    digitalWrite(DIR_A, motorDirection);    // Set motor direction
+    analogWrite(PWM_A, commandSPD);     // Set the speed of the motor, 255 is the maximum value
+    retval = true;
+  }
+
+  return retval;
+}
+
 void setup() {
+  // Configure the A output
+  pinMode(BRAKE_A, OUTPUT);  // Brake pin on channel A
+  pinMode(DIR_A, OUTPUT);    // Direction pin on channel A
+
+  // Open Serial communication
+  Serial.begin(115200);
+  Serial.println("Toy Car Demo:");
+
+  myservo.attach(SERVO_A);
+
+  // set prescaler for Timer 3 (pin 3) to 1 to get 31372.55 Hz
+  TCCR3B = (TCCR3B & 0b11111000) | 0x01;
+
+    // Itialize BLE UART
+  Serial3.begin(9600);
+
+  // Set BLE's name
+  Serial3.write("AT NAMERCcar\r\n");
+  //Serial.write("Zadejte prikaz AT: \n\n");
+
+//  analogReference(INTERNAL1V1);
+}
+
+void loop() {
+  command_t incommingByte;
+  int desiredSPD = 0;
+  int oldSPD = 0;
+  int desiredServo = SERVO_CENTER;
+  int oldServo = SERVO_CENTER;
+  long int lastMillis = 0;
+  long int currentMillis = 0;
+
+  while(true)
+  {
+    incommingByte = command_decoder_fastest(Serial3); //Serial.read();
+    //if(incommingByte != -1)
+    //{
+      switch(incommingByte)
+      {
+        case UP:
+          if(desiredSPD < MAX_SPD)
+          {
+            desiredSPD += SPD_INCREMENT;
+
+          }
+        break;
+        case DOWN:
+          if(desiredSPD > -MAX_SPD)
+          {
+            desiredSPD -= SPD_INCREMENT;
+          }
+        break;
+        case RIGHT:
+          if(desiredServo > SERVO_MIN_RIGHT)
+          {
+            desiredServo -= SERVO_INCREMENT;
+          }
+        break;
+        case LEFT:
+          if(desiredServo < SERVO_MAX_LEFT)
+          {
+            desiredServo += SERVO_INCREMENT;
+          }
+        break;
+        case UP_TRIANGLE:
+          desiredServo = SERVO_CENTER;
+        break;
+        default:
+          desiredSPD = 0; //Stop the engine
+          //desiredServo = SERVO_CENTER;
+          //myservo.write(45);
+        //Serial.println(incommingByte);
+      }
+
+      if(oldSPD != desiredSPD)
+      {
+        oldSPD = desiredSPD;
+        motorCMD(desiredSPD);
+
+        //Serial.println(desiredSPD);
+      }
+
+      if(oldServo != desiredServo)
+      {
+        oldServo = desiredServo;
+        myservo.write(desiredServo);
+
+        //Serial.println(desiredServo);
+      }
+    //}
+
+    currentMillis = millis();
+    if(currentMillis - lastMillis > STATUS_PRINT_INTERVAL_MS)
+    {
+      lastMillis = currentMillis;
+      Serial.println(analogRead(SNS_BATTERY_VLTG)); // * 5.0 / 1023.0);
+
+      //Serial.println(analogRead(SNS_A));
+      //TODO: print status - voltages, currents etc.
+    }
+  }
+}
+
+
+/*void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
@@ -20,4 +168,4 @@ void loop() {
   
   
   Serial.write(command_decoder(Serial3));
-}
+}*/
