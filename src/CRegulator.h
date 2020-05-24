8671a06f6ec34bae9de35c75fb5e7d606c3b45fd
Ladapn <2048042+ladapn@users.noreply.github.com>
2020-05-24 19:02:55 +0200

    Clarification on limitation of exp filter added

--- src/CRegulator.h
+++ src/CRegulator.h
@@ -41,6 +41,7 @@ class CExpFilter
                 //=BITRSHIFT(BITLSHIFT(F3, 2) - F3 + C5 + 2, 2)
                 // 3/4 * state + 1/4 * input
                 // (4*state - state + input + rounding_constatnt) / 4
+                // WARNING: This never converges to zero! It gets stuck at 2
                 m_state = ((m_state << 2) - m_state + input + 2) >> 2; 
             }
                 
