b1dcda81fbf022cf33902d60135977611092d3d6
Ladapn <2048042+ladapn@users.noreply.github.com>
2020-12-30 22:21:09 +0100

    Add Status and Motion packets and XOR CRC

--- src/BLEPackets.cpp
+++ src/BLEPackets.cpp
@@ -8,8 +8,44 @@ size_t BLE_printer::BLE_print_US_data(byte id, unsigned long tick, unsigned long
     us_data.sonar_data = sonar_data;
     us_data.tick = tick;
     
-    // TODO add CRC computation
-    us_data.crc = 0;
+    // TODO add CRC_and_send function
+    us_data.crc = compute_xor_CRC((uint8_t*)&us_data, sizeof(us_data));
     
     return m_BLE_serial.write((uint8_t*)&us_data, sizeof(us_data));
+}
+
+size_t BLE_printer::BLE_print_status_data(byte id, unsigned long tick, unsigned int battery_adc)
+{
+    status_packet_t status_data;
+
+    status_data.id = id;
+    status_data.tick = tick;
+    
+    status_data.battery_voltage_adc = battery_adc;
+    status_data.total_currnet_adc = 0; // FIXME
+    status_data.motor_currnet_adc = 0; // FIXME
+
+    // TODO add CRC computation
+    status_data.crc = compute_xor_CRC((uint8_t*)&status_data, sizeof(status_data));
+    
+    return m_BLE_serial.write((uint8_t*)&status_data, sizeof(status_data));
+}
+
+// TODO: verify this does not introduce too much delay to control loop
+byte BLE_printer::compute_xor_CRC(const uint8_t *buffer, size_t size)
+{     
+    byte CRC = 0;
+
+    if(size)
+    {
+        CRC = buffer[0];
+
+        // (size - 1) -> don't include the last byte - the CRC byte
+        for(unsigned int i = 1; i < (size - 1); i++)
+        {
+            CRC ^= buffer[i];
+        }
+    }
+
+    return CRC;
 }
--- src/BLEPackets.h
+++ src/BLEPackets.h
@@ -4,8 +4,9 @@
 #include <Arduino.h>
 
 enum us_packet_ids {FRONT_US_ID = 100, RIGHT_FRONT_US_ID = 101, RIGHT_CENTER_US_ID = 102, RIGHT_BACK_US_ID = 103};
+enum util_packet_ids {STATUS_ID = 80, MOTION_CMD_ID = 90};
 
-typedef struct sonar_packet{
+typedef struct sonar_packet {
     byte id;
     unsigned long tick;
     unsigned long sonar_data;
@@ -13,6 +14,31 @@ typedef struct sonar_packet{
     } sonar_packet_t;
 
 // status packet, battery voltage, current, motor current? version ID?
+typedef struct status_packet {
+    byte id;
+    unsigned long tick;
+    unsigned long version_ID; // long -> 4 bytes, instead of 8 chars
+    unsigned int battery_voltage_adc; // analogRead() returns int; int is 2 bytes long on ATmega
+    unsigned int total_currnet_adc;
+    unsigned int motor_currnet_adc;
+    byte crc;
+
+    // FIXME probably not the best way of doing it
+    status_packet()
+    {
+        version_ID = GIT_REV;
+    };
+    } status_packet_t;
+
+// motion command packet - srv and spd command
+typedef struct motion_command_packet {
+    byte id;
+    unsigned long tick;
+    unsigned int servo_cmd; // int appropriate here? 
+    signed int motor_cmd; // int appropriate here? 
+    byte crc;
+
+} motion_command_packet_t;
 
 
 class BLE_printer
@@ -21,6 +47,8 @@ class BLE_printer
 public:
     BLE_printer(HardwareSerial &serial) : m_BLE_serial(serial) {};
     size_t BLE_print_US_data(byte id, unsigned long tick, unsigned long sonar_data);
+    size_t BLE_print_status_data(byte id, unsigned long tick, unsigned int battery_adc);
+    byte compute_xor_CRC(const uint8_t *buffer, size_t size);
 
 };
 
