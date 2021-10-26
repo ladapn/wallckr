0f3c6edc28435d9cf37a85cc6f41edfb6c4a4003
Ladapn <2048042+ladapn@users.noreply.github.com>
2021-10-26 22:13:49 +0200

    Add draft of Sensing class

---
+++ include/Sensing.h
@@ -0,0 +1,42 @@
+#ifndef SENSING_H
+#define SENSING_H
+
+#include <Arduino.h>
+#include "UltraSoundSensor.h"
+
+class Sensing
+{
+    // battery related constants 
+    static const int BATTERY_CELLS = 8;
+    static const int BATTERY_CUTTOFF_ADC = (0.9 / 5 * 1024 * BATTERY_CELLS) / 16;
+    static const int SNS_BATTERY_VLTG = A8;
+
+    // sonar related constants
+    static const int TRIGGER_PIN_FRONT = 32;  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+    static const int ECHO_PIN_FRONT = 33;  // Arduino pin tied to echo pin on the ultrasonic sensor.
+
+    static const int TRIGGER_PIN_RIGHT_FRONT = 34;
+    static const int ECHO_PIN_RIGHT_FRONT = 35;
+
+    static const int TRIGGER_PIN_RIGHT_CENTER = 36;
+    static const int ECHO_PIN_RIGHT_CENTER = 37;
+
+    static const int MAX_DISTANCE = 200; // TODO: rename to MAX_DISTANCE_CM
+
+    UltraSoundSensor sonar_front; //(TRIGGER_PIN_FRONT, ECHO_PIN_FRONT, MAX_DISTANCE);
+    UltraSoundSensor sonar_right_front; //(TRIGGER_PIN_RIGHT_FRONT, ECHO_PIN_RIGHT_FRONT, MAX_DISTANCE);
+    UltraSoundSensor sonar_right_center; //(TRIGGER_PIN_RIGHT_CENTER, ECHO_PIN_RIGHT_CENTER, MAX_DISTANCE);
+
+public:
+    // TODO reference to printer? 
+    Sensing() : sonar_front(TRIGGER_PIN_FRONT, ECHO_PIN_FRONT, MAX_DISTANCE),
+                sonar_right_front(TRIGGER_PIN_RIGHT_FRONT, ECHO_PIN_RIGHT_FRONT, MAX_DISTANCE),
+                sonar_right_center(TRIGGER_PIN_RIGHT_CENTER, ECHO_PIN_RIGHT_CENTER, MAX_DISTANCE) {};
+    
+    bool battery_voltage_ok(); 
+    unsigned int get_side_distance_cm();
+    unsigned int get_front_distance_cm();
+
+};
+
+#endif // SENSING_H
---
+++ src/Sensing.cpp
@@ -0,0 +1,47 @@
+#include "Sensing.h"
+
+bool Sensing::battery_voltage_ok()
+{
+    auto battery_voltage_adc = analogRead(SNS_BATTERY_VLTG);
+
+    if(battery_voltage_adc <= BATTERY_CUTTOFF_ADC)
+    {
+        return false;        
+    } 
+
+    return true; 
+}
+
+unsigned int Sensing::get_front_distance_cm()
+{
+    // Fire front sonar
+    auto front_sonar_cm = sonar_front.get_distance_filtered_cm();
+
+    //BLE_out.BLE_print_US_data(FRONT_US_ID, currentMillis, front_sonar_cm);
+
+    return front_sonar_cm; 
+}
+
+unsigned int Sensing::get_side_distance_cm()
+{
+    
+
+    // Fire right front sonar      
+    auto right_front_sonar_cm = sonar_right_front.get_distance_raw_cm(); 
+    auto right_sonar_cm = right_front_sonar_cm;
+
+    //BLE_out.BLE_print_US_data(RIGHT_FRONT_US_ID, currentMillis, right_front_sonar_cm);
+
+    // Fire right center sonar 
+    auto right_center_sonar_cm = sonar_right_center.get_distance_raw_cm();
+
+    if(right_center_sonar_cm < right_sonar_cm)
+    {
+       right_sonar_cm = right_center_sonar_cm;
+    }
+
+    //BLE_out.BLE_print_US_data(RIGHT_CENTER_US_ID, currentMillis, right_center_sonar_cm);   
+
+    return right_sonar_cm; 
+}
+
