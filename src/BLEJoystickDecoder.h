8258e05319fef6133e0d2ecd1f4e2f1c9d526446
Ladapn <2048042+ladapn@users.noreply.github.com>
2022-07-30 23:53:18 +0200

    Minor refactoring of BLEJoystickDecoder.h

--- src/BLEJoystickDecoder.h
+++ src/BLEJoystickDecoder.h
@@ -1,13 +1,13 @@
 #ifndef BLEJOYSTICKDECODER_H
 #define BLEJOYSTICKDECODER_H
 
-#include "Arduino.h"
+class Stream; 
 
 enum command_t {NO_COMMAND = 0, UP, RIGHT, DOWN, LEFT, UP_TRIANGLE, RIGHT_CIRCLE, DOWN_X, LEFT_SQUARE }; 
 
 class BLEJoystickDecoder
 {
-    HardwareSerial &m_cmdSerial;
+    Stream &m_cmdSerial;
     command_t input_to_command(char in);
     command_t command_decoder();
     command_t command_decoder_fast();
@@ -15,7 +15,7 @@ class BLEJoystickDecoder
 
 public: 
 
-    BLEJoystickDecoder(HardwareSerial &cmdSerial) : m_cmdSerial(cmdSerial) {};
+    BLEJoystickDecoder(Stream &cmdSerial) : m_cmdSerial(cmdSerial) {};
     bool check_motion_cmd(int &desired_speed, int &desired_servo_angle);
 };
 #endif // BLEJOYSTICKDECODER_H
