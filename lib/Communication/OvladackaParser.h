2f746ed424d85485eccf0f89b92e009804674603
Ladapn <2048042+ladapn@users.noreply.github.com>
2022-10-02 23:53:34 +0200

    Refactor OvladackaParser
    Decrease scope of "current" local variable

--- lib/Communication/OvladackaParser.h
+++ lib/Communication/OvladackaParser.h
@@ -11,16 +11,15 @@ public:
     JoystickCommand decode(Stream &input_stream) override
     {
         JoystickCommand command = JoystickCommand::NO_COMMAND; 
-        char current;
         const int DIFF_LOWER_UPPER = 'a' - 'A';
 
         if(input_stream.available()) 
         {  
-            current = input_stream.read();
+            auto current = input_stream.read();
 
             if(current < 'a') // if uppercase
             {
-            current += DIFF_LOWER_UPPER;  // make it lower case
+                current += DIFF_LOWER_UPPER;  // make it lower case
             }
             
             command = input_to_joystick_command(current);
