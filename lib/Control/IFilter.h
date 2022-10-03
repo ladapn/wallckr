6b52b1ba137647e3065c263238941bc6ae2c1735
Ladapn <2048042+ladapn@users.noreply.github.com>
2022-10-03 22:39:09 +0200

    Add doxycoments to IFilter

--- lib/Control/IFilter.h
+++ lib/Control/IFilter.h
@@ -1,10 +1,18 @@
 #ifndef IFILTER_H
 #define IFILTER_H
 
+/**
+ * Interface for data filtering classes
+ */
 template <typename T>
 class IFilter
 {
 public:
+    /**
+     * Get next filter output value
+     * @param[in] input filters input for current step
+     * @return filter output value for next step
+     */
     virtual T next(T input) = 0;
 
 };
