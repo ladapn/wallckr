4bbca574ec05998ac5ca8d89b24509956f99659b
Ladapn <2048042+ladapn@users.noreply.github.com>
2022-10-18 16:12:20 +0200

    Add unit test for sonar packets

--- test/communication/test_robot_packets/test_robot_packets.cpp
+++ test/communication/test_robot_packets/test_robot_packets.cpp
@@ -47,10 +47,38 @@ void test_status_packet(void)
     TEST_ASSERT_EQUAL_MEMORY(expected, actual, sizeof(expected));
 }
 
+void test_sonar_packet(void)
+{    
+    const int PACKET_ID = 101; 
+    const unsigned long CURRENT_TICK = 1000; 
+    const unsigned long MEASUREMENT = 0xdeadbeef;
+
+    SonarPacket sonar_packet(PACKET_ID, CURRENT_TICK, MEASUREMENT); 
+    // NOTE: AVR is little endian, i.e. LSB goes first
+    uint8_t expected[] = {0x65,                     // id 
+                          0xE8, 0x03, 0x00, 0x00,   // tick
+                          0xEF, 0xBE, 0xAD, 0xDE,   // measurement
+                          0xAC};                    // CRC    
+
+    SimulatedUART sim_uart;
+    RobotPrinter printer(sim_uart);
+
+    printer.add_CRC_and_print(reinterpret_cast<uint8_t*>(&sonar_packet), sizeof(sonar_packet));
+
+    uint8_t actual[sizeof(expected)];
+    for(unsigned i = 0; i < sizeof(expected); i++)
+    {
+        actual[i] = sim_uart.read();
+    }    
+    
+    TEST_ASSERT_EQUAL_MEMORY(expected, actual, sizeof(expected));
+}
+
 int main( int argc, char **argv) {
     UNITY_BEGIN();
    
     RUN_TEST(test_status_packet);
+    RUN_TEST(test_sonar_packet);
     
     UNITY_END();
 
