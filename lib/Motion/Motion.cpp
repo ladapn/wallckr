63965df0670756704cf1fb0e806349e8f0944d16
Ladapn <2048042+ladapn@users.noreply.github.com>
2023-07-01 17:33:54 +0200

    Refactor Motion to use early returns
    Introduced to decrease indentation and make the code more readable.

--- lib/Motion/Motion.cpp
+++ lib/Motion/Motion.cpp
@@ -2,51 +2,54 @@
 
 bool Motion::setSpeed(int speed)
 {
-    bool retval = false;
-
-    if (!disabled)
+    if (disabled)
     {
-      if (prev_speed != speed)
-      {
-          prev_speed = speed;
-
-          auto motorDirection = MotorDirection::FORWARD; // HIGH -> forward
-
-          if ((speed <= MAX_SPEED) && (speed >= -MAX_SPEED))
-          {
-              if (speed < 0)
-              {
-                  motorDirection = MotorDirection::BACKWARD;
-                  speed = abs(speed);
-              }
-
-              digitalWrite(DIR_A, static_cast<uint8_t>(motorDirection)); // Set motor direction
-              analogWrite(PWM_A, speed);             // Set the speed of the motor, 255 is the maximum value
-
-              retval = true;
-          }
-      }
+        return false;
+    }
+    
+    if (prev_speed == speed)
+    {
+        return false;
     }
 
-    return retval;
+    if ((speed > MAX_SPEED) && (speed < -MAX_SPEED))
+    {
+        return false;
+    }
+    
+    prev_speed = speed;
+    auto motorDirection = MotorDirection::FORWARD; // HIGH -> forward
+
+    if (speed < 0)
+    {
+        motorDirection = MotorDirection::BACKWARD;
+        speed = abs(speed);
+    }
+
+    digitalWrite(DIR_A, static_cast<uint8_t>(motorDirection)); // Set motor direction
+    analogWrite(PWM_A, speed);             // Set the speed of the motor, 255 is the maximum value
+
+    return true;
 }
 
 bool Motion::setAngle(int angle)
 {
-    bool retval = false;
-
-    if (!disabled)
+    if (disabled)
     {
-      if (oldServo != angle)
-      {
-          oldServo = angle;
-          steering_servo.write(angle);
-
-          retval = true;
-      }
+        return false;
+    }
+    
+    if (oldServo != angle)
+    {
+        oldServo = angle;
+        steering_servo.write(angle);
+    }
+    else
+    {
+        return false; 
     }
 
-    return retval; 
+    return true; 
 }
 
 bool Motion::set_speed_and_angle(int speed, int angle)
