d8b4de10a09571df4261fd49e2d9ffefdfb648cd
Ladapn <2048042+ladapn@users.noreply.github.com>
2026-05-09 09:44:27 +0200

    Fix motor test name

--- tests/lib/Motion/Motor/src/test_motor.cpp
+++ tests/lib/Motion/Motor/src/test_motor.cpp
@@ -3,23 +3,23 @@
 
 ZTEST_SUITE(motor_tests, NULL, NULL, NULL, NULL, NULL);
 
-ZTEST(exp_fileter_tests, test_step_N_4_int)
+ZTEST(motor_tests, basic_motor_test)
 {
     Motor motor;
     motor.initialize();
 
     int spd = -100;
     while(true) {
-        for(spd = -100; spd <= 100; spd+=5) {
-        motor.set_speed(spd);
-        k_msleep(1000);
-    }
+        for(spd = -100; spd <= 100; spd += 5) {
+            motor.set_speed(spd);
+            k_msleep(1000);
+        }
 
-    for(spd = 100; spd >= -100; spd-=5) {
-        motor.set_speed(spd);
-        k_msleep(1000);
+        for(spd = 100; spd >= -100; spd -= 5) {
+            motor.set_speed(spd);
+            k_msleep(1000);
+        }
     }
-  }
 }
 
 
