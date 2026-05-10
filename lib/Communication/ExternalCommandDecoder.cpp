1ba8f64e3d5024a94ba89e5be8ae2cbb10d415ab
ladapn <2048042+ladapn@users.noreply.github.com>
2026-05-10 11:39:26 +0200

    Fix boundary checks for incoming commands; minor refactor; enable additional compiler warnings (#16)
    * Fix command boundary checks in ExternalCommandDecoder
    
    * Refactor command checking logic in ExternalCommandDecoder for clarity
    
    * Update build flags in platformio.ini for enhanced warning visibility

--- lib/Communication/ExternalCommandDecoder.cpp
+++ lib/Communication/ExternalCommandDecoder.cpp
@@ -8,49 +8,47 @@ bool ExternalCommandDecoder::check_external_command(RobotCommand &robot_command)
 {
     auto incoming_cmd = input_decoder.decode(cmd_stream);
 
-    if(incoming_cmd != JoystickCommand::NO_COMMAND)
+    if(incoming_cmd == JoystickCommand::NO_COMMAND)
     {
-      switch(incoming_cmd)
-      {
-        case JoystickCommand::UP:
-          if(robot_command.desired_speed < MAX_SPEED)
-          {
-            robot_command.desired_speed += SPEED_INCREMENT;
-          }
-        break;
-        case JoystickCommand::DOWN:
-          if(robot_command.desired_speed > -MAX_SPEED)
-          {
-            robot_command.desired_speed -= SPEED_INCREMENT;
-          }
-        break;
-        case JoystickCommand::RIGHT:
-          if(robot_command.desired_servo_angle > SERVO_MIN_RIGHT)
-          {
-            robot_command.desired_servo_angle -= SERVO_INCREMENT;
-          }
-        break;
-        case JoystickCommand::LEFT:
-          if(robot_command.desired_servo_angle < SERVO_MAX_LEFT)
-          {
-            robot_command.desired_servo_angle += SERVO_INCREMENT;
-          }
-        break;
-        case JoystickCommand::UP_TRIANGLE:
-          robot_command.desired_servo_angle = SERVO_CENTER;
-        break;
-        case JoystickCommand::RIGHT_CIRCLE:
-          robot_command.enable_automatic_operation = !robot_command.enable_automatic_operation;
-        break;
-        default:
-          robot_command.desired_speed = 0; 
-      }
-    }
-    else
-    {
-      return false; 
+      return false;
     }
 
-    return true; 
+    switch(incoming_cmd)
+    {
+      case JoystickCommand::UP:
+        if(robot_command.desired_speed + SPEED_INCREMENT <= MAX_SPEED)
+        {
+          robot_command.desired_speed += SPEED_INCREMENT;
+        }
+        break;
+      case JoystickCommand::DOWN:
+        if(robot_command.desired_speed - SPEED_INCREMENT >= -MAX_SPEED)
+        {
+          robot_command.desired_speed -= SPEED_INCREMENT;
+        }
+        break;
+      case JoystickCommand::RIGHT:
+        if(robot_command.desired_servo_angle - SERVO_INCREMENT >= SERVO_MIN_RIGHT)
+        {
+          robot_command.desired_servo_angle -= SERVO_INCREMENT;
+        }
+        break;
+      case JoystickCommand::LEFT:
+        if(robot_command.desired_servo_angle + SERVO_INCREMENT <= SERVO_MAX_LEFT)
+        {
+          robot_command.desired_servo_angle += SERVO_INCREMENT;
+        }
+        break;
+      case JoystickCommand::UP_TRIANGLE:
+        robot_command.desired_servo_angle = SERVO_CENTER;
+        break;
+      case JoystickCommand::RIGHT_CIRCLE:
+        robot_command.enable_automatic_operation = !robot_command.enable_automatic_operation;
+        break;
+      default:
+        robot_command.desired_speed = 0;
+    }
+
+    return true;
 
 }
