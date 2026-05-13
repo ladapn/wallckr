e5ce13c33838924ac4633277fbece660327fe3bd
Ladapn <2048042+ladapn@users.noreply.github.com>
2026-05-13 13:52:05 +0200

    Introduce serialization methods for SonarPacket

--- lib/Communication/RobotPackets.cpp
+++ lib/Communication/RobotPackets.cpp
@@ -1,8 +1,45 @@
 #include "RobotPackets.h"
 #include "IRobotIOStream.h"
 
+namespace {
+    void append_uint8(uint8_t *buffer, size_t &offset, uint8_t value)
+    {
+        buffer[offset++] = value;
+    }
+
+    void append_uint16_le(uint8_t *buffer, size_t &offset, uint16_t value)
+    {
+        buffer[offset++] = static_cast<uint8_t>(value & 0xFF);
+        buffer[offset++] = static_cast<uint8_t>((value >> 8) & 0xFF);
+    }
+
+    void append_uint32_le(uint8_t *buffer, size_t &offset, uint32_t value)
+    {
+        buffer[offset++] = static_cast<uint8_t>(value & 0xFF);
+        buffer[offset++] = static_cast<uint8_t>((value >> 8) & 0xFF);
+        buffer[offset++] = static_cast<uint8_t>((value >> 16) & 0xFF);
+        buffer[offset++] = static_cast<uint8_t>((value >> 24) & 0xFF);
+    }
+}
+
+bool SonarPacket::serialize(uint8_t *buffer, size_t buffer_size) const
+{
+    if(buffer == nullptr || buffer_size < SERIALIZED_SIZE)
+    {
+        return false;
+    }
+
+    size_t offset = 0;
+    append_uint8(buffer, offset, id);
+    append_uint32_le(buffer, offset, tick);
+    append_uint32_le(buffer, offset, sonar_data);
+    append_uint32_le(buffer, offset, 0); // CRC is reserved for RobotPrinter - it will be overwritten there
+
+    return true;
+}
+
 uint8_t RobotPrinter::compute_xor_CRC(const uint8_t *buffer, size_t size)
-{     
+{
     uint8_t CRC = 0;
 
     if(size)
@@ -20,7 +57,7 @@ uint8_t RobotPrinter::compute_xor_CRC(const uint8_t *buffer, size_t size)
 }
 
 size_t RobotPrinter::add_CRC_and_print(uint8_t *buffer, size_t size)
-{    
+{
 
     buffer[size - 1] = compute_xor_CRC(buffer, size);
     return robot_serial.write(buffer, size);
--- lib/Communication/RobotPackets.h
+++ lib/Communication/RobotPackets.h
@@ -5,18 +5,20 @@
 #include <stdint.h>
 
 /**
- * enum defining all possible IDs of ultrasound measurement packets 
+ * enum defining all possible IDs of ultrasound measurement packets
  */
 enum us_packet_ids {FRONT_US_ID = 100, RIGHT_FRONT_US_ID = 101, RIGHT_CENTER_US_ID = 102, RIGHT_BACK_US_ID = 103};
 /**
- * enum defining all possible IDs of utility packets 
+ * enum defining all possible IDs of utility packets
  */
 enum util_packet_ids {STATUS_ID = 80, MOTION_CMD_ID = 90};
 
 /**
- * Structure representing output packet to hold measurements from ultrasound sensors 
+ * Structure representing output packet to hold measurements from ultrasound sensors
  */
 struct SonarPacket {
+
+    static constexpr size_t SERIALIZED_SIZE = 10;
     uint8_t id;
     uint32_t tick;
     uint32_t sonar_data;
@@ -25,7 +27,7 @@ struct SonarPacket {
      * Constructor method
      * @param[in] packet_id ID of ultrasound packet, it differs based on sensor location - see \ref us_packet_ids
      * @param[in] tick_ms current system time in ms
-     * @param[in] measurement ultrasound measurement 
+     * @param[in] measurement ultrasound measurement
      */
     SonarPacket(uint8_t packet_id, uint32_t tick_ms, uint32_t measurement) :
     id(packet_id),
@@ -33,10 +35,18 @@ struct SonarPacket {
     sonar_data(measurement)
     {
     }
+
+    /**
+     * Serialize packet fields in the wire format. CRC field is reserved for RobotPrinter.
+     * @param[out] buffer chunk of data to hold serialized packet
+     * @param[in] buffer_size size of buffer in bytes, it should be at least SERIALIZED_SIZE
+     * @return true if serialization was successful, false otherwise
+     */
+    bool serialize(uint8_t *buffer, size_t buffer_size) const;
 };
 
 /**
- * Structure representing status packet - packet holding information about robot's status 
+ * Structure representing status packet - packet holding information about robot's status
  */
 struct StatusPacket {
     uint8_t id = STATUS_ID;
@@ -52,7 +62,7 @@ struct StatusPacket {
      * @param[in] tick_ms current system time in ms
      * @param[in] battery_voltage current battery voltage in ADC units
      * @param[in] total_current total current drawn from battery in mA
-     * @param[in] motor_current total current drawn by motor in mA 
+     * @param[in] motor_current total current drawn by motor in mA
      */
     StatusPacket(uint32_t tick_ms, uint16_t battery_voltage, uint16_t total_current, uint16_t motor_current) :
     tick(tick_ms),
@@ -65,20 +75,20 @@ struct StatusPacket {
 };
 
 /**
- * Structure representing motion command packet -> packet sent back when a motion command is received from user 
+ * Structure representing motion command packet -> packet sent back when a motion command is received from user
  */
 struct MotionCommandPacket {
     uint8_t id = MOTION_CMD_ID;
     uint32_t tick;
     uint16_t servo_cmd;
-    int16_t motor_cmd; 
+    int16_t motor_cmd;
     uint8_t crc = 0;
     /**
      * Constructor method
      * @param[in] tick_ms current system time in ms
      * @param[in] servo_command desired servo angle in deg
      * @param[in] motor_command desired motor speed on scale from -255 to 255, where 255 is full speed
-     * and negative numbers mean going backward 
+     * and negative numbers mean going backward
     */
     MotionCommandPacket(uint32_t tick_ms, uint16_t servo_command, int16_t motor_command) :
     tick(tick_ms),
@@ -90,16 +100,16 @@ struct MotionCommandPacket {
 class IRobotIOStream;
 
 /**
- * Class to send out robot packets 
+ * Class to send out robot packets
  */
 class RobotPrinter
 {
     IRobotIOStream &robot_serial;
     /**
-     * Compute CRC by doing xor of all bytes together 
+     * Compute CRC by doing xor of all bytes together
      * @param[in] buffer chunk of data containing packet
      * @param[in] size size of packet in bytes
-     * @return computed CRC 
+     * @return computed CRC
      */
     uint8_t compute_xor_CRC(const uint8_t *buffer, size_t size);
 public:
@@ -110,10 +120,10 @@ public:
     explicit RobotPrinter(IRobotIOStream &serial) : robot_serial(serial) {};
     /**
      * Compute packet's CRC and send it out
-     * @param[in] buffer chunk of data containing packet 
+     * @param[in] buffer chunk of data containing packet
      * @param[in] size size of packet in bytes
-     * @return number of bytes written 
-     */    
+     * @return number of bytes written
+     */
     size_t add_CRC_and_print(uint8_t *buffer, size_t size);
 
 };
