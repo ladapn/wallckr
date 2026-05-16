c28a7e870b1bb679c6022d4df3ebd107cd6089e9
Ladapn <2048042+ladapn@users.noreply.github.com>
2026-05-16 09:54:42 +0200

    Introduce battery sensor interface

---
+++ lib/Sensing/IBatterySensor.h
@@ -0,0 +1,25 @@
+#ifndef IBATTERYSENSOR_H
+#define IBATTERYSENSOR_H
+
+/**
+ * Interface for battery voltage sensor
+ */
+class IBatterySensor
+{
+public:
+    /**
+     * Method to get battery voltage in ADC units
+     * @return battery voltage in ADC units
+     */
+    virtual uint16_t get_battery_voltage_adc() = 0;
+
+    /**
+     * Method to get battery voltage in volts
+     * @return battery voltage in volts
+     */
+    virtual uint16_t get_battery_voltage_V() = 0;
+
+    virtual ~IBatterySensor() = default;
+};
+
+#endif // IBATTERYSENSOR_H
