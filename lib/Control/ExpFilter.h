fe3bdf372f6d2632abd8c6b69c0a9374de978dd0
Ladapn <2048042+ladapn@users.noreply.github.com>
2026-05-19 12:22:43 +0200

    Fix filter code style

--- lib/Control/ExpFilter.h
+++ lib/Control/ExpFilter.h
@@ -3,63 +3,49 @@
 
 #include "IFilter.h"
 
-template <typename T, int N>
-struct ExpFilterMath;
+template <typename T, int N> struct ExpFilterMath;
 
-template <int N>
-struct ExpFilterMath<int, N>
-{
-    static int compute(int input, int state)
-    {
-        return static_cast<int>(((N - 1L) * state + input) / N);
-    }
+template <int N> struct ExpFilterMath<int, N> {
+  static int compute(int input, int state) {
+    return static_cast<int>(((N - 1L) * state + input) / N);
+  }
 };
 
-template <int N>
-struct ExpFilterMath<float, N>
-{
-    static float compute(float input, float state)
-    {
-        constexpr float input_weight = 1.0f / N;
-        constexpr float state_weight = 1.0f - input_weight;
+template <int N> struct ExpFilterMath<float, N> {
+  static float compute(float input, float state) {
+    constexpr float input_weight = 1.0f / N;
+    constexpr float state_weight = 1.0f - input_weight;
 
-        return state_weight * state + input_weight * input;
-    }
+    return state_weight * state + input_weight * input;
+  }
 };
 
 /**
  * Class implementing exponential filter template
  */
-template <typename T, int N> class ExpFilter : public IFilter<T>
-{
+template <typename T, int N> class ExpFilter : public IFilter<T> {
 private:
-    T m_state;
-    bool m_first_step;
-    T core(T input)
-    {
-        return ExpFilterMath<T, N>::compute(input, m_state);
-    }
-public:
-    ExpFilter() : m_state(0), m_first_step(true) {};
-    /**
-     * Get next filter output value
-     * @param[in] input filters input for current step
-     * @return filter output value for next step
-     */
-    T next(T input) override
-    {
-        if(m_first_step)
-        {
-            m_first_step = false;
-            m_state = input;
-        }
-        else
-        {
-            m_state = core(input);
-        }
+  T m_state;
+  bool m_first_step;
+  T core(T input) { return ExpFilterMath<T, N>::compute(input, m_state); }
 
-        return m_state;
+public:
+  ExpFilter() : m_state(0), m_first_step(true) {};
+  /**
+   * Get next filter output value
+   * @param[in] input filters input for current step
+   * @return filter output value for next step
+   */
+  T next(T input) override {
+    if (m_first_step) {
+      m_first_step = false;
+      m_state = input;
+    } else {
+      m_state = core(input);
     }
+
+    return m_state;
+  }
 };
 
 #endif
