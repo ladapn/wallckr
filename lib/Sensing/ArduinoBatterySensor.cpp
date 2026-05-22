6e5e942d7b581289bfb4981aa7cbb330a51e4ebf
Ladapn <2048042+ladapn@users.noreply.github.com>
2026-05-22 15:28:04 +0200

    Fix overflow when converting ADC value to mVDC

--- lib/Sensing/ArduinoBatterySensor.cpp
+++ lib/Sensing/ArduinoBatterySensor.cpp
@@ -8,5 +8,6 @@ uint16_t ArduinoBatterySensor::get_battery_voltage_adc() {
 }
 
 uint16_t ArduinoBatterySensor::get_battery_voltage_mVDC() {
-  return analogRead(SNS_BATTERY_VLTG) * ADC_REFERENCE_mV / ADC_MAX;
+  // static cast to uint32_t to avoid overflow during multiplication
+  return static_cast<uint32_t>(analogRead(SNS_BATTERY_VLTG)) * ADC_REFERENCE_mV / ADC_MAX;
 }
